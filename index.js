15da97130888bc8b376186e89d3555848e79ad64
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-03-16 20:29:54 +0100

    .

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function example1() {
 
   log(new Date().toISOString(), "ERROR", "Example 1.2: An exception occurred");
 
+  // Log 1 second later to check the timestamp is different:
   setTimeout(function () {
     log(new Date().toISOString(), "INFO", "Example 1.3: 1 second later...");
   }, 1000);
@@ -33,6 +34,7 @@ function example2() {
   logInformation("Example 2.1: This is an informational message");
   logError("Example 2.2: An exception occurred!");
 
+  // Log 1 second later to check the timestamp is different:
   setTimeout(function () {
     logInformation("Example 2.3: 1 second later...");
   }, 1000);
@@ -55,12 +57,17 @@ function example3() {
   // const log = (datetime) => (severity) => (message) =>
   //   console.log(`${datetime} [${severity}] - ${message}`);
 
-  const logInformation = log(new Date().toISOString())("INFO");
-  const logError = log(new Date().toISOString())("ERROR");
+  var timestamp = {
+    toString: () => new Date().toISOString()
+  };
+
+  const logInformation = log(timestamp)("INFO");
+  const logError = log(timestamp)("ERROR");
 
   logInformation("Example 3.1: This is an informational message!");
   logError("Example 3.2: An exception occurred!");
 
+  // Log 1 second later to check the timestamp is different:
   setTimeout(function () {
     logInformation("Example 3.3: 1 second later...");
   }, 1000);
@@ -80,31 +87,35 @@ function example4() {
   }
 
   function log(datetime, severity, message) {
-    if (!datetime) datetime = new Date().toISOString();
     console.log(`${datetime} [${severity}] - ${message}`);
   }
+
+  var timestamp = {
+    toString: () => new Date().toISOString()
+  };
 
   let curriedLog = curry(log);
 
   // We can call log with all 3 arguments:
   curriedLog(
-    new Date().toISOString(),
+    timestamp,
     "INFO",
     "Example 4.1: This is an informational message"
   );
 
   // We can pass all arguments separately:
-  curriedLog(new Date().toISOString())("INFO")(
+  curriedLog(timestamp)("INFO")(
     "Example 4.2: This is an informational message"
   );
 
   // And we can apply partial application:
-  const logInformation = curriedLog(undefined)("INFO");
+  const logInformation = curriedLog(timestamp)("INFO");
   logInformation("Example 4.3: This is an informational message");
 
+  // Log 1 second later to check the timestamp is different:
   setTimeout(function () {
     logInformation("Example 4.4: 1 second later...");
-  }, 2000);
+  }, 1000);
 
   // etc. etc.
 }
